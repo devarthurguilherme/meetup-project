64aac505f09e82c6d21827cc2fc94370afb20a62
devarthurguilherme <arthurguilherme2013@gmail.com>
2023-10-17 07:05:40 +0100

    Testing Chakra Templates

--- arthur-guilherme/src/App.jsx
+++ arthur-guilherme/src/App.jsx
@@ -3,12 +3,14 @@ import { FaCoffee, FaBeer } from 'react-icons/fa';
 import { motion } from "framer-motion"
 
 import Home from "./pages/home"
+import SideBar from "./components/SideBar"
 
 
 
 function App() {
   return (
     <>
+    <SideBar />
     <Home />
     </>
   )
