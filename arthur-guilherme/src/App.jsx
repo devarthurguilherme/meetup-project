96dbf73ea82760647bd7e57ed0f051c677f6d52c
devarthurguilherme <arthurguilherme2013@gmail.com>
2023-10-17 07:12:11 +0100

    Fixing Section

--- arthur-guilherme/src/App.jsx
+++ arthur-guilherme/src/App.jsx
@@ -1,8 +1,3 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { FaCoffee, FaBeer } from 'react-icons/fa';
-import { motion } from "framer-motion"
-
-import Home from "./pages/home"
 import SideBar from "./components/SideBar"
 
 
@@ -10,8 +5,7 @@ import SideBar from "./components/SideBar"
 function App() {
   return (
     <>
-    <SideBar />
-    <Home />
+      <SideBar />
     </>
   )
 }
