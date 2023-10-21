dacc93d2eb244119729406879a33c703ed66ca42
devarthurguilherme <arthurguilherme2013@gmail.com>
2023-10-21 12:03:46 +0100

    Creation of the Popular Section

---
+++ arthur-guilherme/src/components/Upcomming/Events.js
@@ -0,0 +1,13 @@
+import Upcommingone from '../../assets/upcomming-one.png';
+import Upcommingtwo from '../../assets/upcomming-two.jpg';
+import Upcommingthree from '../../assets/upcomming-three.jpg';
+import Upcommingfour from '../../assets/upcomming-four.jpg';
+
+const Events = [
+    { id: 1, image: Upcommingone, dateEvent:"Sat, Aug 14 @ 9:45 PM NPT", },
+    { id: 2, image: Upcommingtwo, dateEvent:"Sat, Aug 14 @ 9:45 PM NPT", },
+    { id: 3, image: Upcommingthree, dateEvent:"Sat, Aug 14 @ 9:45 PM NPT", },
+    { id: 4, image: Upcommingfour, dateEvent:"Sat, Aug 14 @ 9:45 PM NPT", },
+];
+
+export default Events;
